1232e888f2c51215512de72359dded970e49c56b
0x2V <0x2V369@gmail.com>
2024-07-28 16:18:01 +0300

    Added new symbols to symbols list

--- password_generator.py
+++ password_generator.py
@@ -10,7 +10,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 # List of symbols to be used in password generation
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '?']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '?', '-', ',', '.', '[', ']']
 
 # Welcome message to the user
 print("Welcome to the PyPassword Generator!")
